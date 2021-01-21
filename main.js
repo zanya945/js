8ec2697f7778859d3d4874c75e011bfdabb8a72a
zanya945 <77784793+zanya945@users.noreply.github.com>
2021-01-21 21:52:32 +0800

    mian

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-document.getElementById("demo").innerHTML = "w";
-document.getElementById("demo").innerHTML= "J";
-document.
+document.getElementById("wang").innerHTML = "w";
+document.getElementById("jin").innerHTML = "J";
+document.getElementById("demo").style.fontSize = "35px";
+document.getElementByld("隱藏").style.display = "none";
+document.getElementById("顯示").style.display = "block";
+document.getElementById("var").innerHTML = 5 + 6;
+document.write(5 + 6);
+var id = "var";
