c8ec7cdb4e0920ee19d629a39a64378ef6a86271
zanya945 <77784793+zanya945@users.noreply.github.com>
2021-01-26 19:55:03 +0800

    main

--- main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("wang").innerHTML = "w";
-document.getElementById("jin").innerHTML = "J";
-document.getElementById("demo").style.fontSize = "35px";
-document.getElementByld("隱藏").style.display = "none";
-document.getElementById("顯示").style.display = "block";
-document.getElementById("var").innerHTML = 5 + 6;
-document.write(5 + 6);
-var id = "var";
